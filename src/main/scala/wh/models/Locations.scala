fa55582c2971e7d6a1fecd2fc9b5dc34f3d40a80
giovannini <giovannini.thomas@gmail.com>
2015-11-27 16:55:05 +0100

    Ajout d'images

--- src/main/scala/wh/models/Locations.scala
+++ src/main/scala/wh/models/Locations.scala
@@ -10,10 +10,16 @@ object Locations {
   val current =
     Locations(
       List(
-        Location("Canal Saint-Martin", description = "Un endroit bien sympathique pour prendre une bière au bord de l'eau."),
-        Location("Le Mal Barré", description = "Burgers originaux, potatoes et cupcakes faits maison proposés dans un cadre chaleureux avec terrasse sur rue."),
-        Location("Tour Eiffel", description = "Impossible de visiter Paris sans passer devant."),
-        Location("Les Champs Élysées", description = "La plus belle avenue de Paris. Si si.")
+        Location("Canal Saint-Martin",
+          description = "Un endroit bien sympathique pour prendre une bière au bord de l'eau.",
+          "http://www.de-nicher.com/wp-content/uploads/sites/5/old/6a01156fa90739970c01a511a54d5b970c-pi.jpg"
+        ),
+        Location("Le Mal Barré",
+          description = "Burgers originaux, potatoes et cupcakes faits maison proposés dans un cadre chaleureux avec terrasse sur rue.",
+          "http://www.frankizbird.com/wp-content/uploads/2014/03/DSCF20671.jpg"
+        ),
+        Location("Tour Eiffel", description = "Impossible de visiter Paris sans passer devant.", "http://www.kairn.com/files/news/42475312222826-tour-eiffel-paris-jpg"),
+        Location("Les Champs Élysées", description = "La plus belle avenue de Paris. Si si.", "http://www.holiday-velvet.com/guide/wp-content/uploads/2013/01/champs-elysees.jpg")
       )
     )
 
--- src/main/scala/wh/models/domain.scala
+++ src/main/scala/wh/models/domain.scala
@@ -2,7 +2,7 @@ package wh.models
 
 case class User(name: String)
 
-case class Location(name: String, description: String)
+case class Location(name: String, description: String, imageUrl: String = "")
 
 case class Rating(user: User, location: Location, rate: Double) {
   require (rate >= 0 && rate <= 5)
