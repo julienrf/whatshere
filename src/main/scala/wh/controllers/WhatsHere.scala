7e50171be401d24ee031d38acfa6cae6f6f8ee19
giovannini <giovannini.thomas@gmail.com>
2015-11-27 10:19:59 +0100

    Templates basiques

--- src/main/scala/wh/controllers/WhatsHere.scala
+++ src/main/scala/wh/controllers/WhatsHere.scala
@@ -7,17 +7,20 @@ class WhatsHere extends Controller {
 
   /** (temporary) Show all registered locations */
   val list = Authenticated {
-    NotImplemented
+    val list = Seq("Chez moi", "Chez ma maman", "Dans un bar")
+    Ok(wh.html.list(list))
   }
 
   /** Show the list of recommended locations for the current user */
   val recommendations = Authenticated {
-    NotImplemented
+    val list = Seq("Chez moi", "Chez ma maman", "Dans un bar")
+    Ok(wh.html.recommandation(list))
   }
 
   /** Show the details of one location, so that the user can like it */
   def show(locationName: String) = Authenticated {
-    NotImplemented
+
+    Ok(wh.html.show(locationName))
   }
 
   /** Add the fact that the user rated the given location */
